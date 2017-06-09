2878ff7c194148c3aad1afa6dfabdd19a77c952a
notwogly <1542063282@qq.com>
2017-06-10 00:34:04 +0800

    Interpreter.h

---
+++ Interpreter.h
@@ -0,0 +1,31 @@
+#ifndef Interpreter_h
+#define Interpreter_h
+#include "base.h"
+
+class Interpreter{
+private:
+    std::string querys;//query string
+public:
+	Interpreter();
+	~Interpreter();
+	void inputQuery();
+    void EXEC();
+    void EXEC_CREATE();
+    void EXEC_CREATE_TABLE();
+    void EXEC_CREATE_INDEX();
+    void EXEC_DROP();
+	void EXEC_SELECT();
+    void EXEC_INSERT();
+    void EXEC_DELETE();
+    void EXEC_PRINT();
+    void EXEC_EXIT();
+    void EXEC_FILE();
+    
+    vector<Condition> ConditionList(TableStruct& table );
+    void Normolize();
+};
+
+bool InverttoInt(string s, int& x);
+bool InvertToFloat(string s, float& x);
+
+#endif /* Interpreter_h */
