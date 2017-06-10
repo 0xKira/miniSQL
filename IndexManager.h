1b2b994722f24ff068163803938ff4eedced4781
Lin Junxiong <linjunxiong1997@gmail.com>
2017-06-10 22:41:22 +0800

    index header

--- IndexManager.h
+++ IndexManager.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <fstream>
+#include "Basis.h"
+#include "TableStruct.h"
+#include "BufferManager.h"
+#include "BufferUnit.h"
+
+class IndexManager
+{
+	IndexManager() {};
+	~IndexManager() {};
+	void createIndex(const Attribute& attr, std::string indexName, const TableStruct& table);
+	void dropIndex(std::string indexName, const TableStruct& table);
+	vector<tuple> exactSearchWithIndex(std::string tableName, std::string indexName, const Data& value);
+	vector<tuple> rangeSearchWithIndex(std::string tableName, std::string indexName, const Data& down, const Data& up);
+	void insertNew(const Data& value);
+	void deleteOld(const Data& value);
+};
