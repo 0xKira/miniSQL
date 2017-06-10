aacaebe4a654a8f678a2457a64135b022717698a
Lin Junxiong <linjunxiong1997@gmail.com>
2017-06-10 18:35:13 +0800

    API header

--- API.h
+++ API.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "Basis.h"
+#include "TableStruct.h"
+#include "Catalog.h"
+#include "BufferManager.h"
+#include "IndexManager.h"
+
+class API
+{
+public:
+	API() {};
+	~API() {};
+	void createTable(TableStruct&);
+	void dropTable(std::string);
+	void createIndex(std::string);
+	void dropIndex(std::string);
+	void select(const Attribute& attr, std::string tableName, vector<Condition>& condTable);
+	void insertData(std::string tableName, vector<Data>& data);
+	void deleteData(std::string tableName);
+	void deleteData(std::string tableName, vector<Condition>& condTable);
+};
