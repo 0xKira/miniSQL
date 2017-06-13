f89828efc4db32b44e954245f365bfdb6b766340
Kira-cxy <864786842@qq.com>
2017-06-13 14:59:57 +0800

    complete all the functions

--- BufferManager.h
+++ BufferManager.h
@@ -1,5 +1,5 @@
 //
-// Created by Kira on 2017/6/9.
+// Created by Kira on 2017/6/10.
 //
 
 #ifndef MINISQL_BUFFERMANAGER_H
@@ -7,25 +7,31 @@
 
 #include <vector>
 #include <string>
+#include "BufferUnit.h"
 
 using namespace std;
 
 // 管理所有表的buffer
 class BufferManager {
 private:
+    vector<BufferUnit> buffers;
+
     size_t blockSize;
     size_t blockNum;
+
+private:
+    inline int getTableIndex(string tableName);
 
 public:
     bool createTable(string tableName);
     bool deleteTable(string tableName);  // 除了删除文件还需要释放程序中相关buffer管理结构内存
-    bool readBlockData(string tableName, size_t blockIndex, char *outBuffer);
-    bool writeBlockData(string tableName, size_t blockIndex, char *inBuffer);
+    // 下面的blockIndex是指文件中的index
+    bool readBlockData(string tableName, size_t blockIndexInFile, char *outBuffer);
+    bool writeBlockData(string tableName, size_t blockIndexInFile, char *inBuffer);
     bool deleteLastBlockOfFile(string tableName);
-    bool setBlockLock(string tableName, int blockIndex, bool lock);
+    bool enlargeFile(string tableName);
 
-    BufferManager(size_t blockSize = 4096, size_t blockNum = 256) : blockSize(blockSize), blockNum(blockNum) {
-    };
+    BufferManager(size_t blockSize, size_t blockNum);
 };
 
 #endif //MINISQL_BUFFERMANAGER_H
