6cbe85e3d3167fdc8027578f64de63bf2e32c068
notwogly <1542063282@qq.com>
2017-06-19 02:07:09 +0800

    Update Catalog.h

--- Catalog.h
+++ Catalog.h
@@ -1,19 +1,29 @@
 #ifndef Catalog_h
 #define Catalog_h
-#include "Basis.h"
-#include "TableStruct.h"
+#include<map>
+#include<string>
+#include"Basis.h"
+#include"TableStruct.h"
 using namespace std;
 
-class Catalog{ 
-public:
-	Catalog();
-	~Catalog();
-	void addTable(TableStruct& tableinfo);
-	bool hasTable(const string& tablename);
-	void deleteTable(const string& tablename);
-	TableStruct getTable(const string& tablename);
-	void addIndex(const string& tablename, const string& indexname, const string& attriname);
-	void deleteIndex(const string& indexname);
+class Catalog {
+	public:
+		Catalog();
+		~Catalog();
+		void addTable(TableStruct& tableinfo);
+		bool hasTable(const string& tablename);
+		void deleteTable(const string& tablename);
+		TableStruct getTable(const string& tablename);
+		void addIndex(const string& tablename, const string& indexname, const string& attriname);
+		void deleteIndex(const string& indexname);
+
+	public:
+		string bufCat;
+		string bufInd;
+		map<string, int> cat;
+		map<string, int> cat_index;
+		string catFile="Cat.bin";
+		string catIndFile="CatIndex.bin";
 };
 
 #endif /* Catalog_h */
