61a056371fe5c153738f16da8e6047a978b91927
Lin Junxiong <linjunxiong1997@gmail.com>
2017-06-19 02:08:27 +0800

    Update indexmanager

--- IndexManager.cpp
+++ IndexManager.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include "IndexManager.h"
+#include "BpTree.h"
 using namespace std;
 
 void IndexManager::createIndex(const Attribute & attr, std::string indexName, const TableStruct & table)
-{
-	ofstream file;
-	file.open("/data/" + table.tableName + ".index");
-
+{	
+	BpTree index(table, indexName, attr);
+	
 }
