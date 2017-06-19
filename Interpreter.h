20fbe5247c79e961b6fe55dfb1da3534607139e5
notwogly <1542063282@qq.com>
2017-06-20 02:31:45 +0800

    Update Interpreter.h

--- Interpreter.h
+++ Interpreter.h
@@ -1,31 +1,42 @@
 #ifndef Interpreter_h
 #define Interpreter_h
-#include "Basis.h"
+#include<iostream>
+#include<string>
+#include<stdexcept>
+#include<sstream>
+#include<fstream>
+#include<cstdlib>
+#include<vector>
+#include"Basis.h"
+#include"TableStruct.h"
+#include"Catalog.h"
+#include"API.h"
 
-class Interpreter{
-private:
-    std::string querys;//query string
-public:
-	Interpreter();
-	~Interpreter();
-	void inputQuery();
-    void EXEC();
-    void EXEC_CREATE();
-    void EXEC_CREATE_TABLE();
-    void EXEC_CREATE_INDEX();
-    void EXEC_DROP();
-	void EXEC_SELECT();
-    void EXEC_INSERT();
-    void EXEC_DELETE();
-    void EXEC_PRINT(TableStruct &table);
-    void EXEC_EXIT();
-    void EXEC_FILE();
-    
-    vector<Condition> ConditionList(TableStruct& table, string where);
-    Tuple TupleList(TableStruct& table, string where);
+class Interpreter {
+//private:
+	public:
+		std::string querys;//query string
+	public:
+		Interpreter();
+		~Interpreter();
+		void inputQuery();
+		void EXEC();
+		void EXEC_CREATE();
+		void EXEC_CREATE_TABLE();
+		void EXEC_CREATE_INDEX();
+		void EXEC_DROP();
+		void EXEC_SELECT();
+		void EXEC_INSERT();
+		void EXEC_DELETE();
+		void EXEC_PRINT(TableStruct &table);
+		void EXEC_EXIT();
+		void EXEC_FILE();
+
+		vector<Condition> ConditionList(TableStruct& table, string where);
+		Tuple TupleList(TableStruct& table, string where);
 };
 
-bool InverttoInt(string s, int& x);
-bool InvertToFloat(string s, float& x);
+bool invertToInt(string s, int& x);
+bool invertToFloat(string s, float& x);
 
 #endif /* Interpreter_h */
